848d33157d17ebd896e31d32cbc559068480ebd8
araujosp <gabrieloyama14@gmail.com>
2026-04-09 10:53:19 -0300

    arquivo data.php criado

---
+++ data.php
@@ -0,0 +1,53 @@
+<?php
+
+$categorias= [
+    'bruto' =>'bruto',
+    'ferramentas' =>'ferramentas',
+    'acabamento' =>'acabamento'
+];
+
+$produtos_base= [
+    [
+        'id'=>'1',
+        'nome'=>'cimento',
+        'preco'=>38.90,
+        'qtd-estoque' =>173,
+        'categoria'=>'bruto'
+    ],
+    [
+        'id'=>'2',
+        'nome'=>'saco de areia',
+        'preco'=>10.00,
+        'qtd-estoque' =>520,
+        'categoria'=> 'bruto'
+    ],
+    [
+        'id'=>'3',
+        'nome'=>'martelo',
+        'preco'=>50.00,
+        'qtd-estoque' =>65,
+        'categoria'=> 'ferramentas'
+    ],
+    [
+        'id'=>'4',
+        'nome'=>'fita métrica',
+        'preco'=>27.99,
+        'qtd-estoque' =>43,
+        'categoria'=> 'ferramentas'
+    ],
+    [
+        'id'=>'5',
+        'nome'=>'fita métrica',
+        'preco'=>27.99,
+        'qtd-estoque' =>43,
+        'categoria'=> 'ferramentas'
+    ],
+          [
+        'id'=>'5',
+        'nome'=>'concreto pronto',
+        'preco'=>15.25,
+        'qtd-estoque' =>43,
+        'categoria'=> 'ferramentas'
+    ],
+    
+];
