426766fbd6cbe044b1d329d7f970739ef6609cfb
araujosp <gabrieloyama14@gmail.com>
2026-04-10 10:45:17 -0300

    tabela dinãmina puxando dados da array

--- data.php
+++ data.php
@@ -13,70 +13,70 @@ $produtos_base= [
         'id'=>1,
         'nome'=>'cimento',
         'preco'=>38.90,
-        'qtd-estoque' =>173,
+        'qtd_estoque' =>173,
         'categoria'=>'bruto'
     ],
     [
         'id'=>2,
         'nome'=>'saco de areia',
         'preco'=>10.00,
-        'qtd-estoque' =>520,
+        'qtd_estoque' =>520,
         'categoria'=> 'bruto'
     ],
     [
         'id'=>3,
         'nome'=>'martelo',
         'preco'=>50.00,
-        'qtd-estoque' =>65,
+        'qtd_estoque' =>65,
         'categoria'=> 'ferramentas'
     ],
     [
         'id'=>4,
         'nome'=>'furadeira',
         'preco'=>279.99,
-        'qtd-estoque' =>24,
+        'qtd_estoque' =>24,
         'categoria'=> 'ferramentas'
     ],
     [
         'id'=>5,
         'nome'=>'fita métrica',
         'preco'=>27.99,
-        'qtd-estoque' =>43,
+        'qtd_estoque' =>43,
         'categoria'=> 'ferramentas'
     ],
     [
         'id'=>6,
         'nome'=>'concreto pronto',
         'preco'=>15.25,
-        'qtd-estoque' =>43,
+        'qtd_estoque' =>43,
         'categoria'=> 'bruto'
     ],
     [
         'id'=>7,
         'nome'=>'rejunte',
         'preco'=>23.79,
-        'qtd-estoque' =>59,
+        'qtd_estoque' =>59,
         'categoria'=> 'acabamento'
     ],
     [
         'id'=>8,
         'nome'=>'massa corrida',
         'preco'=>82.99,
-        'qtd-estoque' =>55,
+        'qtd_estoque' =>55,
         'categoria'=> 'acabamento'
     ],
     [
         'id'=>9,
         'nome'=>'bloco de concreto',
         'preco'=>4,
-        'qtd-estoque' =>1279,
+        'qtd_estoque' =>1279,
         'categoria'=> 'bruto'
     ],
     [
         'id'=>10,
         'nome'=>'martelete',
         'preco'=>579.79,
-        'qtd-estoque' =>33,
+        'qtd_estoque' =>33,
         'categoria'=> 'ferramentas'
     ],
     
--- index.php
+++ index.php
@@ -8,112 +8,77 @@ $ids = array_column($_SESSION['produtos'], 'id');
 $novoId = $ids ? max($ids) + 1: 1;
 
 $_SESSION['produtos'][] = [
-         'id' => $novoId,
-        'nome' => $_POST['nome'],
-        'preco' => $_POST['preco'],
-        'categoria' => $_POST['categoria']  
-    ];
-    }
-     print '<pre>';
-     print_r($_SESSION['produtos']);
-     print '</pre>';
-?>
+    'id' => $novoId,
+    'nome' => $_POST['nome'],
+    'preco' => $_POST['preco'],
+    'categoria' => $_POST['categoria'],
+    'qtd_estoque' => $_POST['qtd_estoque']
+];
+
+}
+    print '<pre>';
+    print_r($_SESSION['produtos']);
+    print '</pre>';
+
+    ?>
 
 
 <html lang="pt-br">
-<head>
-    <link rel="stylesheet" href="styles.css">
-    <link rel="icon" href="imagens/logo-sem-fundo.png">
-    <title>ConstruTech</title>
-</head>
-<body>
-    <header class="menu">
-        <img src="imagens/logo-sem-fundo.png">
-        <div class="title-header">
-            <img src="imagens/estoque-pronto.png" alt="">
-            <h2>controle de estoque</h2>
-        </div>
-    </header>
-    <main>
-        <section class="list_estoque">
-            <table class="estoque">
-                <tr class="linha_destaque">
-                    <th>Nome</th>
-                    <th>Categoria</th>
-                    <th>Quantidade</th>
-                    <th>Preço Unit.</th>
-                    <th>Valor Total</th>
-                    <th>Status</th>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td>Barra de Ferro</td>
-                    <td>Bruto</td>
-                    <td>30</td>
-                    <td>R$ 15,00</td>
-                    <td>R$ 450,00</td>
+    <head>
+        <link rel="stylesheet" href="styles.css">
+        <link rel="icon" href="imagens/logo-sem-fundo.png">
+        <title>ConstruTech</title>
+    </head>
+    <body>
+        <header class="menu">
+            <img src="imagens/logo-sem-fundo.png">
+            <div class="title-header">
+                <img src="imagens/estoque-pronto.png" alt="">
+                <h2>controle de estoque</h2>
+            </div>
+        </header>
+        <main>
+            <section class="list_estoque">
+                <table class="estoque">
+                    <tr class="linha_destaque">
+                        <th>Nome</th>
+                        <th>Categoria</th>
+                        <th>Quantidade</th>
+                        <th>Preço Unit.</th>
+                        <th>Valor Total</th>
+                        <th>Status</th>
+                        <td></td>
+                    </tr>
+
+                    <?php foreach ($_SESSION['produtos'] as $produto) {
+                        ?>
+                    <tr>
+                    <td><?php echo $produto['nome']?></td>
+                    <td><?php echo $produto['categoria']?></td>
+                    <td><?php echo $produto['qtd_estoque']?></td>
+                    <td><?php echo $produto['preco']?></td>
+                    <td><?php echo $produto['preco'] * $produto['qtd_estoque'] ?></td>
                     <td>Critico</td>
                     <td><img src="./img/trash.png" alt="" class="lixo_icon"></td>
                 </tr>
-                <tr>
-                    <td>Furadeira</td>
-                    <td>Ferramenta</td>
-                    <td>7</td>
-                    <td>R$ 70,00</td>
-                    <td>R$ 490,00</td>
-                    <td>Médio</td>
-                    <td><img src="./img/trash.png" alt="" class="lixo_icon"></td>
-                </tr>
-                <tr>
-                    <td>Gesso</td>
-                    <td>Acabamento</td>
-                    <td>30</td>
-                    <td>R$ 20,00</td>
-                    <td>R$ 600,00</td>
-                    <td>Normal</td>
-                    <td><img src="./img/trash.png" alt="" class="lixo_icon"></td>
-                </tr>
-                <tr>
-                    <td>Enxada</td>
-                    <td>Ferramenta</td>
-                    <td>15</td>
-                    <td>R$ 10,00</td>
-                    <td>R$ 150,00</td>
-                    <td>Normal</td>
-                    <td><img src="./img/trash.png" alt="" class="lixo_icon"></td>
-                </tr>
-                <tr>
-                    <td>Massa Corrida 30kg</td>
-                    <td>Tintas House</td>
-                    <td>30</td>
-                    <td>R$ 100,00</td>
-                    <td>R$ 3.000,00</td>
-                    <td>Critico</td>
-                    <td><img src="./img/trash.png" alt="" class="lixo_icon"></td>
-                </tr>
-                <tr>
-                    <td>Porcelanato 45x45</td>
-                    <td>Acabamento</td>
-                    <td>120</td>
-                    <td>R$ 80,00</td>
-                    <td>R$ 9.600,00</td>                    <td>Medio</td>
-                    <td><img src="./img/trash.png" alt="" class="lixo_icon"></td>
-                </tr>
+                <?php
+                    }
+                ?>
             </table>
         </section>
         <section class="list_incluir_item">
             <h1>+ Adicionar Novo Item</h1>
-            <form action="">
+            <form action="index.php" method = 'POST'>
                 <label for="">Nome *</label>
                 <input type="text" placeholder="Ex: Furadeira" name="produto">
                 <label for="">Categoria *</label>
                 <select name="categoria" id="" name="categoria">
-                    <option value="">Bruto</option>
-                    <option value="">Acabamento</option>
-                    <option value="">Ferramenta</option>
+                    <option value="bruto">Bruto</option>
+                    <option value="Acabamento">Acabamento</option>
+                    <option value="Feramenta">Ferramenta</option>
                 </select>
                 <label for="">Quantidade *</label>
-                <input type="text" placeholder="0" name="quantidade">
+                <input type="text" placeholder="0" name="qtd_estoque">
                 <label for="">Preço Unitário (R$)</label>
                 <input type="text" placeholder="0,00" name="preco">
                 <input type="submit" value="+ Adicionar ao Estoque" name="adicionar_item">
